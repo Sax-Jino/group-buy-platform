dd7285c1e364c01ad3cf770befc81105d3ec4349
Sax-Jino <aa164596@gmail.com>
2025-04-27 20:51:50 +0800

    git commit to main

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flask_babel import Babel
@@ -7,6 +8,7 @@ from flask_socketio import SocketIO
 from pyee import EventEmitter
 
 db = SQLAlchemy()
+migrate = Migrate()
 jwt = JWTManager()
 mail = Mail()
 babel = Babel()
