56720f77e653e77623877e3cf1e97877f15d7de1
Sax-Jino <aa164596@gmail.com>
2025-05-01 16:19:02 +0800

    git commit to main

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -2,6 +2,7 @@ from .user import User
 from .product import Product
 from .order import Order
 from .product_review import ProductReview, ProductQA
+from .logistics import LogisticsCompany
 
 # 為了避免循環引用，在這裡不要導入其他模型
-__all__ = ['User', 'Product', 'Order', 'ProductReview', 'ProductQA']
+__all__ = ['User', 'Product', 'Order', 'ProductReview', 'ProductQA', 'LogisticsCompany']
